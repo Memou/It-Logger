c8403b18c7963b041cd45ad426e8246ec741ffc1
Memou <equaco@gmail.com>
2019-07-17 15:14:26 -0400

    Ui mostly done,modals implemented for edit/add techs and logs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
-import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import Logs from './components/logs/Logs';
 
+import AddBtn from './components/layout/AddBtn';
+import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
+import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
+
 const App = () => {
   useEffect(() => {
     //Init materialize JS
@@ -15,6 +22,12 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        
         <Logs />
       </div>
     </Fragment>
